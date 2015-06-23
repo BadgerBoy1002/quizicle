dcbad64f4361bb2bff4973ac898410ef6cfc6d8f
Saleem Rashid <spudowiar@outlook.com>
2015-06-23 22:44:17 +0100

    quiz: basic rewrite

--- quiz.js
+++ quiz.js
@@ -1,31 +1,11 @@
-   alert("WELCOME to Quizicle!")
-var qOne = prompt("Enter a question!");
-var myqOne = prompt("Enter an answer!");
-   alert("The test is about to begin!");
-var aOne = prompt(qOne);
-if(aOne === myqOne) {
-   alert("Correct!");
-}
-else
-{
-   alert("Wrong!");
-}
-var option = prompt("Would you like to give feedback, yes or no?");
-if(option === "yes") {
-   var feedback = prompt("How would you rate this out of 10?!");
-}
-else
-{
-   alert("...well it's your loss");
-}
-if(feedback > 6) {
-   alert("Thank you for using this program!");
-}
-else
-{
-    var why = prompt("Why so...");
-}
-if(why === why) {
-   alert("Your feedback is much appreciated!");
-}
-   alert("Thank you for using this program!");
+ui.ask({
+    "question": "Enter a question!",
+    "answer": "Enter an answer!"
+}, function (response) {
+    var question = response.question, answer = response.answer;
+    ui.ask({
+        "answer": question
+    }, function (response) {
+        ui.reset(response.answer == answer ? "Correct!" : "WRONG!");
+    }, "The test has begun!");
+}, "WELCOME to Quizicle!");
